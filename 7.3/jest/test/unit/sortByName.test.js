45b18fea229f6a7f4419699d54387633248e8d92
Ilya Zakharov QAMID-50 <oiska@bk.ru>
2023-06-18 11:49:01 +0300

    change test

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,22 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-    it("Books names should be sorted in ascending order", () => {
-        expect(
-            sorting.sortByName([
-                "Гарри Поттер",
-                "Властелин Колец",
-                "Волшебник изумрудного города",
-            ])
-        ).toEqual([
-            "Властелин Колец",
-            "Волшебник изумрудного города",
-            "Гарри Поттер",
-        ]);
-    });
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  it("Books names should be sorted in ascending order 2", () => {
+    expect(
+      sorting.sortByName(["Каша из топора", "Каша из топора", "Айболит"])
+    ).toEqual(["Айболит", "Каша из топора", "Каша из топора"]);
+  });
 });
